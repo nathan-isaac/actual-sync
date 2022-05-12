016efa464eb0458c5873cfac8422ca64a93a697a
Nathan Isaac <nathanjisaac@gmail.com>
2022-05-11 23:32:21 -0700

    fix: only create init tables if not exists

--- internal/storage/sqlite/migrations/init_account.sql
+++ internal/storage/sqlite/migrations/init_account.sql
@@ -1,14 +1,35 @@
-CREATE TABLE auth
-  (password TEXT PRIMARY KEY);
-CREATE TABLE sessions
-  (token TEXT PRIMARY KEY);
-CREATE TABLE files
-  (id TEXT PRIMARY KEY,
-   group_id TEXT,
-   sync_version SMALLINT,
-   encrypt_meta TEXT,
-   encrypt_keyid TEXT,
-   encrypt_salt TEXT,
-   encrypt_test TEXT,
-   deleted BOOLEAN DEFAULT FALSE,
-   name TEXT);
+CREATE TABLE IF NOT EXISTS auth
+(
+    password TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS sessions
+(
+    token TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS files
+(
+    id
+    TEXT
+    PRIMARY
+    KEY,
+    group_id
+    TEXT,
+    sync_version
+    SMALLINT,
+    encrypt_meta
+    TEXT,
+    encrypt_keyid
+    TEXT,
+    encrypt_salt
+    TEXT,
+    encrypt_test
+    TEXT,
+    deleted
+    BOOLEAN
+    DEFAULT
+    FALSE,
+    name
+    TEXT
+);
