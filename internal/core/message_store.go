15c59b2bf6f30ae42e0f3e9d46d5982332bb31ce
Nathan Isaac <nathanjisaac@gmail.com>
2022-05-16 21:45:28 -0700

    feat: add message store interfaces

---
+++ internal/core/message_store.go
@@ -0,0 +1,22 @@
+package core
+
+type BinaryMessage struct {
+	Timestamp   string
+	IsEncrypted bool
+	Content     []byte
+}
+
+type MessageStore interface {
+	Add(message BinaryMessage) (bool, error)
+	GetSense(timestamp string) ([]*BinaryMessage, error)
+}
+
+type MerkleMessage struct {
+	MerkleId string
+	Merkle   string
+}
+
+type MerkleStore interface {
+	Add(message MerkleMessage) error
+	GetForGroup(groupId string) ([]*MerkleMessage, error)
+}
